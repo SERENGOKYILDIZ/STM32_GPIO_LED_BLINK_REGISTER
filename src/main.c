cde53cb47241fd2f25c7ace5080d3288115800c3
Eren <gokyildizsemieren@gmail.com>
2023-12-10 15:14:08 +0300

    Fixed settings of Clock

--- src/main.c
+++ src/main.c
@@ -3,22 +3,45 @@
 
 void RCC_Config(void)
 {
-	// 8Mhz -> 168Mhz
-	RCC->CR &= ~(1 << 0);
-	RCC->CR |= 1 << 16;
-	while(!(RCC->CR & (1 << 17)));
-	RCC->CR |= 1<<19;
-	RCC->PLLCFGR = 0x00000000;
-	RCC->PLLCFGR |= (1 << 22);
-	RCC->PLLCFGR |= (4<<0);
-	RCC->PLLCFGR |= (168<<6);
-	RCC->PLLCFGR &= ~(1 << 16);
-	RCC->PLLCFGR &= ~(1 << 17);
-	RCC->CR |= (1 << 24);
-	while(!(RCC->CR & (1 << 25)));
-	RCC->CFGR &= ~(1 << 0);
-	RCC->CFGR |= (1 << 1);
-	while(!(RCC->CFGR & (1 << 1)));
+	/*
+	 * @brief:
+	 * We can change a specific byte value with '|=' (OR).
+	 * We can write the new value over the current value with '&=' (AND).
+	 * The '~' sign takes the reverse of the expression.
+	 *
+	 *
+	 *@clock:
+	 *(PLL_M:4, PLL_N:168, PLL_P:2)
+	 *SYSCLK = [(8Mhz/4)*168/2] = 168Mhz
+	 *-//////////////////////
+	 *SYSCLK = 168Mhz
+	 *AHB    = 168Mhz (Prescaler:1)
+	 *APB1   = 42Mhz  (Prescaler:4)
+	 *APB2   = 84Mhz  (Prescaler:2)
+	 *-///////////////////////
+	*/
+
+	//Settings of Opening HSE
+	RCC->CR &= ~(1 << 0);               //->HSION = 0, (HSI=OFF)
+	while((RCC->CR & 0X3));				//->Wait HSI deactivate.
+	RCC->CR |= (1 << 16); 				//->HSEON = 1, (HSE=ON)
+	while(!(RCC->CR & (1 << 17))); 		//->Wait HSE active
+
+	//Settings of PLL
+	RCC->CR |= (1<<19); 				//->CSSON = 1.
+	RCC->PLLCFGR |= (1 << 22); 			//->PLLSRC = 1, (PLL Source = HSE)
+	RCC->PLLCFGR |= (4<<0); 			//->PLL_M = 4
+	RCC->PLLCFGR |= (168<<6); 			//->PLL_N = 168
+	RCC->PLLCFGR |= 0x00; 				//->PLL_P = 2
+
+	//Settings of Prescaler
+	RCC->CFGR = 0x0;                    //->AHB  Prescaler = 1
+	RCC->CFGR |= (5 << 10);             //->APB1 Prescaler = 4
+	RCC->CFGR |= (4 << 13);             //->APB2 Prescaler = 2
+
+	RCC->CR = (1<<19);                  //-> HSERDY Flag clear.
+	RCC->CR = (1<<23);                  //-> CSS Flag clear.
+	SystemCoreClockUpdate();
 }
 void GPIO_Config(void)
 {
@@ -51,8 +74,6 @@ int main(void)
 {
 	//Settings of Clock
 	RCC_Config();
-	SystemCoreClockUpdate();
-
 	//Settings of GPIO
 	GPIO_Config();
 
